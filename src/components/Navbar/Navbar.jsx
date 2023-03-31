3935eba3770cc7a90e4e05a834e083520a971916
yoshimitsuy <rafael.lacana7@gmail.com>
2023-03-31 08:17:02 -0300

    styled components config

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav } from './Navbar.elements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        cu
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
